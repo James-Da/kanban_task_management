79ea09efce9d45ad229aed8b00119b0baed800bd
James D <66693959+James-Da@users.noreply.github.com>
2023-09-01 17:41:29 +0200

    added toggle board

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ import iconUp from "../assets/icon-chevron-up.svg";
 import elipsis from "../assets/icon-vertical-ellipsis.svg";
 import HeaderDropDown from "./HeaderDropDown";
 import AddEditBoardModal from "../modals/AddEditBoardModal";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = ({ boardModalOpen, setBoardModalOpen }) => {
+	const dispatch = useDispatch();
 	const [openDropDown, setOpenDropDown] = useState(false);
 	const [boardType, setBoardType] = useState("add");
+
+	const boards = useSelector((state) => state.boards);
+
+	const board = boards.find((board) => board.isActive);
+	console.log(board);
 	return (
 		<div className="p-4 fixed left-0 bg-white dark:bg-[#2b2c37] right-0 ">
 			<header className="flex justify-between dark:text-white items-center">
@@ -20,7 +27,7 @@ const Header = ({ boardModalOpen, setBoardModalOpen }) => {
 					</h3>
 					<div className="flex items-center">
 						<h3 className="truncate max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans">
-							board name
+							{board.name}
 						</h3>
 						<img
 							src={openDropDown ? iconUp : iconDown}
--- src/components/HeaderDropDown.jsx
+++ src/components/HeaderDropDown.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import boardIcon from "../assets/icon-board.svg";
 import lightIcon from "../assets/icon-light-theme.svg";
 import darkIcon from "../assets/icon-dark-theme.svg";
 
 import { Switch } from "@headlessui/react";
 import useDarkMode from "../Hooks/useDarkMode";
+import boardsSlice from "../redux/boardsSlice";
 
 const HeaderDropDown = ({ setOpenDropDown, setBoardModalOpen }) => {
+	const dispatch = useDispatch();
 	const [colorTheme, setTheme] = useDarkMode();
 	const [darkSide, setDarkSide] = useState(
 		colorTheme === "light" ? true : false
@@ -40,6 +42,9 @@ const HeaderDropDown = ({ setOpenDropDown, setBoardModalOpen }) => {
 							className={`flex items-baseline space-x-2 dark:text-white px-5 py-4 ${
 								board.isActive && "bg-[#635fc7] rounded-r-full text-white mr-8"
 							}`}
+							onClick={() => {
+								dispatch(boardsSlice.actions.setBoardActive({ index }));
+							}}
 						>
 							<img src={boardIcon} alt="board icon" className="h-4" />
 							<p className="text-lg font-bold">{board.name}</p>
